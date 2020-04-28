3581731c515512ab7958845ba15935d579549317
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-28 16:03:22 +0100

    Add test/unit/sonarqube_code_coverage.sh v2.0.5

---
+++ test/unit/sonarqube_code_coverage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Unit test code coverage for SonarQube to cover all modules.
+# This will run the Python code coverage module against all unit test modules.
+# This will show the amount of code that was tested and which lines of code
+#	that was skipped during the test.
+
+coverage erase
+
+echo ""
+echo "Running unit test modules in conjunction with coverage"
+coverage run -a --source=sftp_class test/unit/sftp_class/sftp_init.py
+
+echo ""
+echo "Producing code coverage report"
+coverage combine
+coverage report -m
+coverage xml -i
+

86839059c0c9dee48d29815ff46a4e63ab4b6ef8
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-04-28 16:03:04 +0100

    Add test/unit/sftp_class/unit_test_run.sh v2.0.5

---
+++ test/unit/sftp_class/unit_test_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Unit testing program for the git_class.py module.
+# This will run all the units tests for this program.
+# Will need to run this from the base directory where the module file
+#   is located at.
+
+echo ""
+echo "Unit test:  git_class.py"
+test/unit/sftp_class/sftp_init.py
+
